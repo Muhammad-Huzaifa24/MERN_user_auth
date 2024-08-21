1cb755912f1531c9d5cee9b24d0e54201b275ec5
Muhammad <huzaifakhawar100@gmail.com>
2024-08-21 21:30:10 +0500

    React api url changed

--- front-end/src/services/userService.js
+++ front-end/src/services/userService.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 // const API_URL = "http://localhost:3001/api";
-const API_URL = "https://mern-user-auth.vercel.app/api";
+// const API_URL = "https://mern-user-auth.vercel.app/api";
+const API_URL = process.env.REACT_APP_API_URL_REMOTE;
 
 // signup user
 
