33a172a24c40736bbd14a38ee439846fc295d458
Muhammad <huzaifakhawar100@gmail.com>
2024-08-21 16:42:05 +0500

    cors issue 2

--- back-end/index.js
+++ back-end/index.js
@@ -7,12 +7,9 @@ const cors = require("cors");
 const app = express();
 
 // Middlewares
-const cors = require("cors");
-const corsOptions = {
-  origin: "http://localhost:3000",
-  credentials: true,
-  optionSuccessStatus: 200,
-};
+
+app.use(cors({ origin: "http://localhost:3000" }));
+
 app.use(cors(corsOptions));
 app.use(express.json());
 
